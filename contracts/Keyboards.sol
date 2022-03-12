27c982b62dbf2581e01043cb3c6576d078c412f6
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-12 14:35:14 +1000

    Update contract with create function

--- contracts/Keyboards.sol
+++ contracts/Keyboards.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.4;
 contract Keyboards {
   string[] public createdKeyboards;
 
+  function create(string calldata _description) external {
+    createdKeyboards.push(_description);
+  }
+
   function getKeyboards() view public returns(string[] memory) {
     return createdKeyboards;
   }
