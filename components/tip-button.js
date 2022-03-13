a8eb857e14897ebe5b84dce90ef71347d6e1c9f6
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-13 16:51:28 +1000

    CLeanup tip-button

--- components/tip-button.js
+++ components/tip-button.js
@@ -3,24 +3,17 @@ import SecondaryButton from "./secondary-button";
 import { ethers } from "ethers";
 import abi from "../utils/Keyboards.json"
 
-export default function TipButton({ ethereum, index }) {
-  const contractAddress = '0xefF7380A098C01430d12B9864462bB16443eEb4a';
-  const contractABI = abi.abi;
-
+export default function TipButton({ keyboardsContract, index }) {
   const [mining, setMining] = useState(false)
 
   const submitTip = async (e) => {
-    if (!ethereum) {
-      console.error('Ethereum object is required to submit a tip');
-      return;
+    if (!keyboardsContract) {
+        console.error('KeyboardsContract object is required to submit a tip');
+        return;
     }
 
     setMining(true);
     try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const signer = provider.getSigner();
-      const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
-
       const tipTxn = await keyboardsContract.tip(index, { value: ethers.utils.parseEther("0.01") })
       console.log('Tip transaction started...', tipTxn.hash)
 
