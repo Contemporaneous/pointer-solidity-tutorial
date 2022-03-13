8133c1f5838ec23714db489a699e054554a06101
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-13 12:37:30 +1000

    Setup Keyboard Creation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import { useState, useEffect } from "react";
 import PrimaryButton from "../components/primary-button";
+import abi from "../utils/Keyboards.json"
+import { ethers } from "ethers";
 
 export default function Home() {
 
   const [ethereum, setEthereum] = useState(undefined);
   const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([]);
+  const [newKeyboard, setNewKeyboard] = useState("");
+
+
+  const contractAddress = '0xE0B80bC3837522D4d279789429F01E26E34cC760';
+  const contractABI = abi.abi;
 
   const handleAccounts = (accounts) => {
     if (accounts.length > 0) {
@@ -26,6 +34,19 @@ export default function Home() {
       handleAccounts(accounts);
     }
   };
+  useEffect(() => getKeyboards(), [connectedAccount])
+
+  const getKeyboards = async () => {
+    if (ethereum && connectedAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      const keyboards = await keyboardsContract.getKeyboards();
+      console.log('Retrieved keyboards...', keyboards)
+      setKeyboards(keyboards)
+    }
+  };
   useEffect(() => getConnectedAccount(), []);
 
   const connectAccount = async () => {
@@ -38,6 +59,29 @@ export default function Home() {
     handleAccounts(accounts);
   };
 
+
+  const submitCreate = async (e) => {
+    e.preventDefault();
+
+    if (!ethereum) {
+      console.error('Ethereum object is required to create a keyboard');
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log('Create transaction started...', createTxn.hash);
+
+    await createTxn.wait();
+    console.log('Created keyboard!', createTxn.hash);
+
+    await getKeyboards();
+  }
+
+
   if (!ethereum) {
     return <p>Please install MetaMask to connect to this site</p>
   }
@@ -47,5 +91,25 @@ export default function Home() {
   }
 
 
-  return <p>The start of our app!</p>
+  return (
+    <div className="flex flex-col gap-y-8">
+      <form className="flex flex-col gap-y-2">
+        <div>
+          <label htmlFor="keyboard-description" className="block text-sm font-medium text-gray-700">
+            Keyboard Description
+          </label>
+        </div>
+        <input
+          name="keyboard-type"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          value={newKeyboard}
+          onChange={(e) => { setNewKeyboard(e.target.value) }}
+        />
+        <PrimaryButton type="submit" onClick={submitCreate}>
+          Create Keyboard!
+        </PrimaryButton>
+      </form>
+      <div>{keyboards.map((keyboard, i) => <p key={i}>{keyboard}</p>)}</div>
+    </div>
+  )
 }
