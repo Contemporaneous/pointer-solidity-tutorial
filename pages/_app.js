a7eaee3acf2157a29e53de4bc5d3f62fcc6f7b20
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-13 16:58:37 +1000

    Add metamask component to top level app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
+import MetaMaskAccountProvider from '../components/meta-mask-account-provider'
 import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+      <MetaMaskAccountProvider>
       <Toaster />
       <Component {...pageProps} />
-    </>
+      </MetaMaskAccountProvider>
   )
 }
 
