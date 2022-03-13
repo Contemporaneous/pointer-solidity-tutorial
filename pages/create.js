f81ca03b353cd68404ea9239dec95c3c479557e7
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-13 17:01:51 +1000

    Add metamask context to create

--- pages/create.js
+++ pages/create.js
@@ -6,8 +6,7 @@ import getKeyboardsContract from "../utils/getKeyboardsContract"
 
 export default function Create() {
 
-  const [ethereum, setEthereum] = useState(undefined);
-  const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const { ethereum, connectedAccount, connectAccount } = useMetaMaskAccount();
 
   const [keyboardKind, setKeyboardKind] = useState(0)
   const [isPBT, setIsPBT] = useState(false)
@@ -16,37 +15,8 @@ export default function Create() {
 
   const keyboardsContract = getKeyboardsContract(ethereum);
 
-  const handleAccounts = (accounts) => {
-    if (accounts.length > 0) {
-      const account = accounts[0];
-      console.log('We have an authorized account: ', account);
-      setConnectedAccount(account);
-    } else {
-      console.log("No authorized accounts yet")
-    }
-  };
-
-  const getConnectedAccount = async () => {
-    if (window.ethereum) {
-      setEthereum(window.ethereum);
-    }
-
-    if (ethereum) {
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-      handleAccounts(accounts);
-    }
-  };
+  
   useEffect(() => getConnectedAccount(), []);
-
-  const connectAccount = async () => {
-    if (!ethereum) {
-      alert('MetaMask is required to connect an account');
-      return;
-    }
-
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    handleAccounts(accounts);
-  };
 
   const submitCreate = async (e) => {
     e.preventDefault();
